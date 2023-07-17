240f04d69c9f01e8207e9a12339168ad27e71cf4
jk719 <jamilkabir719@gmail.com>
2023-07-17 17:32:15 -0400

    worked on hamburger icon for dropdown menu

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-//
+
+document.querySelector(".menu-toggle").addEventListener("click", function() {
+var nav = document.querySelector("nav");
+if (nav.style.display === "none") {
+    nav.style.display = "block";
+} else {
+    nav.style.display = "none";
+}
+});
+
