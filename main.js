3b16b36b94c727b22c6e96de2ed82abcb48af0c2
jk719 <jamilkabir719@gmail.com>
2023-07-19 17:42:33 -0400

    fixed mobile menu to hide when a link is clicked

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+
 document.querySelector(".menu-toggle").addEventListener("click", function() {
-    var nav = document.querySelector("nav");
-    if (nav.style.display === "none") {
-        nav.style.display = "block";
-    } else {
-        nav.style.display = "none";
-    }
+var nav = document.querySelector("nav");
+if (nav.style.display === "none") {
+    nav.style.display = "block";
+} else {
+    nav.style.display = "none";
+}
+});
+
+
+document.querySelectorAll("nav a").forEach(function(navLink) {
+    navLink.addEventListener("click", function() {
+        document.querySelector("nav").style.display = "none";
+    });
 });
 
 
