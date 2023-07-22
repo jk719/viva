3918b87dda6abcba9eb9477a77ea972e8d48a3fb
jk719 <jamilkabir719@gmail.com>
2023-07-21 22:26:49 -0400

    Added more sections

--- main.js
+++ main.js
@@ -13,17 +13,23 @@ if (nav.style.display === "none") {
 
 document.querySelectorAll("nav a").forEach(function(navLink) {
     navLink.addEventListener("click", function(event) {
-        event.preventDefault(); // prevent the default action
-        var target = document.querySelector(this.getAttribute('href')); // get the target element
+        var target = this.getAttribute('href');
 
-        // Get the height of the header
-        var headerHeight = document.querySelector('header').offsetHeight;
+        // check if link is external
+        if (!target.startsWith("http")) {
+            event.preventDefault(); // prevent the default action
 
-        // Calculate the position to scroll to by subtracting the header height from the target element's top position
-        var positionToScrollTo = target.offsetTop - headerHeight;
+            target = document.querySelector(target); // get the target element
 
-        // Scroll to the new position
-        window.scrollTo({ top: positionToScrollTo, behavior: 'smooth' });
+            // Get the height of the header
+            var headerHeight = document.querySelector('header').offsetHeight;
+
+            // Calculate the position to scroll to by subtracting the header height from the target element's top position
+            var positionToScrollTo = target.offsetTop - headerHeight;
+
+            // Scroll to the new position
+            window.scrollTo({ top: positionToScrollTo, behavior: 'smooth' });
+        }
 
         // Only hide the navigation menu if the viewport width is <= 600px
         if (window.innerWidth <= 600) {
@@ -31,5 +37,6 @@ document.querySelectorAll("nav a").forEach(function(navLink) {
         }
     });
 });
+
 
 
