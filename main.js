0d1b92afd924bc78b8683805f1497c349804f4e8
Jamil Kabir <jamilkabir719@gmail.com>
2023-09-17 16:11:34 -0400

    Worked on the _more_ link

--- main.js
+++ main.js
@@ -1,53 +1,45 @@
-document.querySelectorAll("nav a").forEach(function(navLink) {
-    navLink.addEventListener("click", function(event) {
-        var target = this.getAttribute('href');
+// Function to handle internal link navigation with smooth scrolling
+function handleInternalLinkClick(event) {
+    var target = this.getAttribute('href');
 
-        // check if link is internal (starts with '#')
-        if (target.startsWith("#")) {
-            event.preventDefault(); // prevent the default action
+    if (target.startsWith("#")) {
+        event.preventDefault();
 
-            target = document.querySelector(target); // get the target element
+        target = document.querySelector(target);
+        if (!target) return; // Exit if target doesn't exist
 
-            // Get the height of the header
-            var headerHeight = document.querySelector('header').offsetHeight;
+        var headerHeight = document.querySelector('header').offsetHeight;
+        var positionToScrollTo = target.offsetTop - headerHeight;
 
-            // Calculate the position to scroll to by subtracting the header height from the target element's top position
-            var positionToScrollTo = target.offsetTop - headerHeight;
+        window.scrollTo({ top: positionToScrollTo, behavior: 'smooth' });
 
-            // Scroll to the new position
-            window.scrollTo({ top: positionToScrollTo, behavior: 'smooth' });
-
-            // Only hide the navigation menu if the viewport width is <= 600px
-            if (window.innerWidth <= 600) {
-                document.querySelector("nav").style.display = "none";
-            }
-        } 
-        // check if link is external (starts with 'http')
-        else if (target.startsWith("http")) {
-            // The default behavior (navigating to the external link) is desired in this case, so we don't need to prevent it or do anything else
+        if (window.innerWidth <= 600) {
+            document.querySelector("nav").style.display = "none";
         }
-        // The link is assumed to be a relative link if it's not an internal or external link
-        else {
-            // The default behavior (navigating to the relative link) is desired in this case, so we don't need to prevent it or do anything else
-        }
-    });
-});
+    }
+}
 
-
-
-
-
-
-document.querySelector(".menu-toggle").addEventListener("click", function() {
+// Function to handle menu toggle on mobile devices
+function toggleMenu() {
     var nav = document.querySelector("nav");
     var header = document.querySelector("header");
 
-    // Get the background color of the header
-    var headerBgColor = window.getComputedStyle(header).getPropertyValue('background-color');
+    if (!nav || !header) return; // Exit if nav or header doesn't exist
 
-    // Apply the header's background color to the nav
+    var headerBgColor = window.getComputedStyle(header).getPropertyValue('background-color');
     nav.style.backgroundColor = headerBgColor;
-    
-    // Toggle the display of the nav
     nav.style.display = (nav.style.display === "none" || nav.style.display === "") ? "block" : "none";
-});
+}
+
+// Add event listeners if elements exist
+var navLinks = document.querySelectorAll("nav a");
+if (navLinks.length) {
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener("click", handleInternalLinkClick);
+    });
+}
+
+var menuToggle = document.querySelector(".menu-toggle");
+if (menuToggle) {
+    menuToggle.addEventListener("click", toggleMenu);
+}
