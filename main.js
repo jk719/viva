a5c3b92a0748d69785ee8d4f6855bcb545b9ba82
Jamil Kabir <jamilkabir719@gmail.com>
2023-09-19 11:09:20 -0400

    Added all jotforms formatted properly. Also fixed nav trasparency on mobile view

--- main.js
+++ main.js
@@ -11,50 +11,61 @@ function handleInternalLinkClick(event) {
         var headerHeight = document.querySelector('header').offsetHeight;
         var positionToScrollTo = target.offsetTop - headerHeight;
 
+        // Smoothly scroll to the target
         window.scrollTo({ top: positionToScrollTo, behavior: 'smooth' });
 
-        if (window.innerWidth <= 600) {
-            document.querySelector("nav").style.display = "none";
-        }
+        // Hide the mobile menu and overlay after a link is clicked
+        hideMobileMenuOverlay();
     }
 }
 
-// Function to handle menu toggle on mobile devices
-// Function to handle menu toggle on mobile devices
-function toggleMenu() {
+// Function to hide mobile menu and overlay
+function hideMobileMenuOverlay() {
     var nav = document.querySelector("nav.navbar");
-    var header = document.querySelector("header");
     var moreDropdown = document.querySelector(".dropdown-menu");
+    var mobileMenuOverlay = document.getElementById("mobile-menu-overlay");
 
-    if (!nav || !header) return; // Exit if nav or header doesn't exist
+    if (!nav) return; // Exit if nav doesn't exist
 
-    if (nav.style.display === "none" || nav.style.display === "") {
-        nav.style.display = "block";
-        moreDropdown.style.display = "block"; // Show the More dropdown menu
-    } else {
-        nav.style.display = "none";
-        moreDropdown.style.display = "none"; // Hide the More dropdown menu
-    }
+    nav.style.display = "none";
+    moreDropdown.style.display = "none"; // Hide the More dropdown menu
+    mobileMenuOverlay.style.display = "none"; // Hide the overlay
 }
-
-
-
-
-
 
 // Add event listeners if elements exist
 var navLinks = document.querySelectorAll("nav a");
 if (navLinks.length) {
     navLinks.forEach(function(navLink) {
-        navLink.addEventListener("click", handleInternalLinkClick);
+        navLink.addEventListener("click", function(event) {
+            if (window.innerWidth <= 600) {
+                toggleMenu(); // Close the menu on mobile devices
+            }
+        });
     });
 }
-
 var menuToggle = document.querySelector(".menu-toggle");
 if (menuToggle) {
     menuToggle.addEventListener("click", toggleMenu);
 }
+// Function to handle menu toggle on mobile devices
+function toggleMenu() {
+    var nav = document.querySelector("nav.navbar");
+    var header = document.querySelector("header");
+    var moreDropdown = document.querySelector(".dropdown-menu");
+    var mobileMenuOverlay = document.getElementById("mobile-menu-overlay");
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
+    if (!nav || !header) return; // Exit if nav or header doesn't exist
 
-
-
+    if (screenWidth <= 600) {
+        if (nav.style.display === "none" || nav.style.display === "") {
+            nav.style.display = "block";
+            moreDropdown.style.display = "block"; // Show the More dropdown menu
+            mobileMenuOverlay.style.display = "block"; // Show the overlay
+        } else {
+            nav.style.display = "none";
+            moreDropdown.style.display = "none"; // Hide the More dropdown menu
+            mobileMenuOverlay.style.display = "none"; // Hide the overlay
+        }
+    }
+}
